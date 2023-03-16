86536c86ce1c5b225346e7da9dc27263bcf0a5a7
evan <evan.lindsay223@gmail.com>
2023-03-15 23:46:03 -0700

    adding tests

--- src/test/java/com/example/Recipes/RecipesApplicationTests.java
+++ src/test/java/com/example/Recipes/RecipesApplicationTests.java
@@ -1,13 +1,208 @@
-package com.example.Recipes;
+//package com.example.Recipes;
+//
+//import com.example.Recipes.Exceptions.NoSuchRecipeException;
+//import com.example.Recipes.Models.Ingredient;
+//import com.example.Recipes.Models.Recipe;
+//import com.example.Recipes.Models.Review;
+//import com.example.Recipes.Models.Step;
+//import com.example.Recipes.Repos.RecipeRepo;
+//import com.example.Recipes.Service.RecipeService;
+//import org.hamcrest.collection.IsCollectionWithSize;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.mock.web.MockHttpServletResponse;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.RequestBuilder;
+//import org.springframework.test.web.servlet.ResultMatcher;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//
+//
+//import java.util.Set;
+//
+//import static javax.swing.UIManager.get;
+//import static org.hamcrest.Matchers.hasSize;
+//import static org.hamcrest.core.StringContains.containsString;
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//class RecipesApplicationTests {
+//	@Autowired
+//	MockMvc mockMvc;
+//	@Autowired
+//	RecipeRepo recipeRepo;
+//
+//
+//	@Test
+//	@Order(1)
+//	public void testGetRecipeByIdSuccessBehavior() throws Exception {
+//		final long recipeId = 1;
+//
+//		//set up GET request
+//		mockMvc.perform(MockMvcRequestBuilders.get("/recipes/" + recipeId))
+//
+//				//print response
+//				.andDo(print())
+//				//expect status 200 OK
+//				.andExpect(status().isOk())
+//				//expect return Content-Type header as application/json
+//				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+//
+//				//confirm returned JSON values
+//				.andExpect(MockMvcResultMatchers.jsonPath("id").value(recipeId))
+//				.andExpect(MockMvcResultMatchers.jsonPath("minutesToMake").value(2))
+//				.andExpect(MockMvcResultMatchers.jsonPath("reviews", IsCollectionWithSize.hasSize(1)))
+//				.andExpect(MockMvcResultMatchers.jsonPath("ingredients", IsCollectionWithSize.hasSize(1)))
+//				.andExpect(MockMvcResultMatchers.jsonPath("steps", IsCollectionWithSize.hasSize(2)));
+//	}
+//	@Test
+//	@Order(2)
+//	public void testGetRecipeByIdFailureBehavior() throws Exception {
+//		final long recipeId = 5000;
+//
+//		//set up guaranteed to fail in testing environment request
+//		mockMvc.perform(MockMvcRequestBuilders.get("/recipes/" + recipeId))
+//
+//				//print response
+//				.andDo(print())
+//				//expect status 404 NOT FOUND
+//				.andExpect(status().isNotFound())
+//				//confirm that HTTP body contains correct error message
+//				.andExpect((ResultMatcher) content().string(containsString("No recipe with ID " + recipeId + " could be found.")));
+//	}
+//	@Test
+//	@Order(3)
+//	public void testGetAllRecipesSuccessBehavior() throws Exception {
+//		//set up get request for all recipe endpoint
+//		mockMvc.perform((RequestBuilder) get("/recipes"))
+//
+//				//expect status is 200 OK
+//				.andExpect(status().isOk())
+//
+//				//expect it will be returned as JSON
+//				.andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON_VALUE))
+//
+//				//expect there are 4 entries
+//				.andExpect((ResultMatcher) jsonPath("$", hasSize(4)))
+//
+//				//expect the first entry to have ID 1
+//				.andExpect((ResultMatcher) jsonPath("$[0].id").value(1))
+//
+//				//expect the first entry to have name test recipe
+//				.andExpect((ResultMatcher) jsonPath("$[0].name").value("test recipe"))
+//
+//				//expect the second entry to have id 2
+//				.andExpect((ResultMatcher) jsonPath("$[1].id").value(2))
+//
+//				//expect the second entry to have a minutesToMake value of 2
+//				.andExpect((ResultMatcher) jsonPath("$[1].minutesToMake").value(2))
+//
+//				//expect the third entry to have id 3
+//				.andExpect((ResultMatcher) jsonPath("$[2].id").value(3))
+//
+//				//expect the third entry to have difficulty rating
+//				.andExpect((ResultMatcher) jsonPath("$[2].difficultyRating").value(5));
+//	}
+//	@Test
+////make sure this test runs last
+//	@Order(11)
+//	public void testGetAllRecipesFailureBehavior() throws Exception {
+//
+//		//delete all entries to force error
+//		recipeRepo.deleteAll();
+//
+//		//perform GET all recipes
+//		mockMvc.perform((RequestBuilder) get("/recipes"))
+//
+//				.andDo(print())
+//
+//				//expect 404 NOT FOUND
+//				.andExpect(status().isNotFound())
+//
+//				//expect error message defined in RecipeService class
+//				.andExpect((ResultMatcher) jsonPath("$").value("There are no recipes yet :( feel free to add one though"));
+//	}
+//	@Test
+//	@Order(4)
+//	public void testCreateNewRecipeSuccessBehavior() throws Exception {
+//		Ingredient ingredient = Ingredient.builder().name("brown sugar").state("dry").amount("1 cup").build();
+//		Step step1 = Step.builder().description("heat pan").stepNumber(1).build();
+//		Step step2 = Step.builder().description("add sugar").stepNumber(2).build();
+//
+//		Review review = Review.builder().description("was just caramel").rating(3).username("idk").build();
+//
+//		Recipe recipe = Recipe.builder()
+//				.name("caramel in a pan")
+//				.difficultyRating(10)
+//				.minutesToMake(2)
+//				.ingredients(Set.of(ingredient))
+//				.steps(Set.of(step1, step2))
+//				.reviews(Set.of(review))
+//				.build();
+//
+//		MockHttpServletResponse response =
+//				mockMvc.perform(post("/recipes")
+//								//set request Content-Type header
+//								.contentType("application/json")
+//								//set HTTP body equal to JSON based on recipe object
+//								.content(TestUtil.convertObjectToJsonBytes(recipe))
+//						)
+//
+//						//confirm HTTP response meta
+//						.andExpect(status().isCreated())
+//						.andExpect(content().contentType("application/json"))
+//						//confirm Location header with new location of object matches the correct URL structure
+//						.andExpect(header().string("Location", containsString("http://localhost/recipes/")))
+//
+//						//confirm some recipe data
+//						.andExpect(jsonPath("id").isNotEmpty())
+//						.andExpect(jsonPath("name").value("caramel in a pan"))
+//
+//						//confirm ingredient data
+//						.andExpect(jsonPath("ingredients", hasSize(1)))
+//						.andExpect(jsonPath("ingredients[0].name").value("brown sugar"))
+//						.andExpect(jsonPath("ingredients[0].amount").value("1 cup"))
+//
+//						//confirm step data
+//						.andExpect(jsonPath("steps", hasSize(2)))
+//						.andExpect(jsonPath("steps[0]").isNotEmpty())
+//						.andExpect(jsonPath("steps[1]").isNotEmpty())
+//
+//						//confirm review data
+//						.andExpect(jsonPath("reviews", hasSize(1)))
+//						.andExpect(jsonPath("reviews[0].username").value("idk"))
+//						.andReturn().getResponse();
+//	}
+//	@Test
+//	@Order(5)
+//	public void testCreateNewRecipeFailureBehavior() throws Exception {
+//
+//		Recipe recipe = new Recipe();
+//
+//		//force failure with empty User object
+//		mockMvc.perform(
+//						post("/recipes")
+//								//set body equal to empty recipe object
+//								.content(TestUtil.convertObjectToJsonBytes(recipe))
+//								//set Content-Type header
+//								.contentType("application/json")
+//				)
+//				//confirm status code 400 BAD REQUEST
+//				.andExpect(status().isBadRequest())
+//				//confirm the body only contains a String
+//				.andExpect(jsonPath("$").isString());
+//	}
+//}
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RecipesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
